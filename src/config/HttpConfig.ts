acc083606172065e5a87aeba35566bb64c0d9f64
Andre Ferreira Trindade <andre.trindade@sensedia.com>
2022-10-10 16:11:20 -0300

    fix - adding repository name

--- src/config/HttpConfig.ts
+++ src/config/HttpConfig.ts
@@ -3,7 +3,6 @@ export default class HttpConfig{
     static UriApi():string{
 
         let uri = "http://localhost:3000";
-
         if(!process.env.DEV){
             uri = "https://fornello.herokuapp.com"
         }
