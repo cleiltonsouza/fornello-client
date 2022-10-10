9636ab1fac746166eb426d0ba2f65a3fbcce5dba
Andre Ferreira Trindade <andre.trindade@sensedia.com>
2022-10-10 15:59:21 -0300

    fix - adding vueRouterMode equals history

--- quasar.conf.js
+++ quasar.conf.js
@@ -47,7 +47,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       publicPath:  process.env.NODE_ENV === "production" ? "/fornello-client/" : "/",
       // transpile: false,
 
