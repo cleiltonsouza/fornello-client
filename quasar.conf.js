f821cce729d50f86c2a8b140b35de737288b38a0
cleiltonsouza <99358968+cleiltonsouza@users.noreply.github.com>
2022-10-11 11:26:34 -0300

    Update quasar.conf.js

--- quasar.conf.js
+++ quasar.conf.js
@@ -47,7 +47,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       publicPath:  process.env.DEV === true ? "/" : "/fornello-client/",
       // transpile: false,
 
