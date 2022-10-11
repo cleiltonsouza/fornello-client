ba799c23447a17067bc56205b39ce7f7efd559e8
cleiltonsouza <99358968+cleiltonsouza@users.noreply.github.com>
2022-10-11 10:56:16 -0300

    Update quasar.conf.js

--- quasar.conf.js
+++ quasar.conf.js
@@ -48,7 +48,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-      // publicPath:  process.env.DEV === true ? "/" : "fornello-client",
+      publicPath:  process.env.DEV === true ? "/" : "/fornello-client/",
       // transpile: false,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
