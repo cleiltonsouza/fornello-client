df9f9ea3cff776825e0b93a9f29e48f0b45214ad
cleiltonsouza <99358968+cleiltonsouza@users.noreply.github.com>
2022-10-11 09:43:28 -0300

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,7 +20,8 @@ export default route<Store<StateInterface>>(function ({ Vue }) {
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
     mode: process.env.VUE_ROUTER_MODE,
-    base: process.env.VUE_ROUTER_BASE
+    base: "fornello-client"
+   // base: process.env.VUE_ROUTER_BASE
   });
 
   return Router;
